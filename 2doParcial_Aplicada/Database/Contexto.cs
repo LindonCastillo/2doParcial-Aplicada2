109a39976f4a29efc91c3b4e37044ce742eb4f35
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-03-11 16:59:42 -0400

    Preparando repositorio para el parcial

---
+++ 2doParcial_Aplicada/Database/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2doParcial_Aplicada.Database
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Database/Parcial2db");
+        }
+    }
+}
